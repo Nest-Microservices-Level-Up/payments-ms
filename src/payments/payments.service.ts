b6363455da1298f268ea12f54aa9dc9632258666
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2026-05-22 16:16:02 -0400

    swfinition of the enviroment variables and the base endpoint to the integration with strip and the generation of the sesion with the stripe sdk

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import Stripe from 'node_modules/stripe/esm/stripe.esm.node';
+import { envs } from 'src/config';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {},
+      },
+
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-Shirt',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancel',
+    });
+
+    return session;
+  }
+}
